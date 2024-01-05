c849766198e60a75935710e3b77115b034770f74
Park <qkrqudwns24@gmail.com>
2024-01-05 13:01:59 +0900

    Changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("김종인");
         System.out.println("싯팔 내가 먼저한다 야발");
+        System.out.println("미안하다. 내가 이 다음에 한다ㅜㅜ");
     }
     
 }
